4cca5b5f2c478360b47729c9c89de0bf18881249
Paul Shanks <paulshankster@gmail.com>
2022-05-28 16:24:59 -0500

    small fixes

--- first_prog.hs
+++ first_prog.hs
@@ -1,12 +1,12 @@
---Main :: IO()
-Main = do
+messyMain :: IO()
+messyMain = do
   print "Who is the mail for?"
   recipient <- getLine
   print "What is the Title?"
   title <- getLine
   print "Who is the Author?"
   author <- getLine
-  print ("Dear " ++ recipient ++ ",\n" ++ "Thanks fot buying " ++ title ++ "\nthanks,\n" ++ author )
+--  print ("Dear " ++ recipient ++ ",\n" ++ "Thanks fot buying " ++ title ++ "\nthanks,\n" ++ author )
   print (createEmail recipient title author)
 
 toPart recipient = "Dear " ++ recipient ++ ",\n"
