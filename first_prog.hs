c378bb411a01140696b91c6e9fe7706867d54b72
Paul Shanks <paulshankster@gmail.com>
2022-05-28 16:12:44 -0500

    updated messy main

--- first_prog.hs
+++ first_prog.hs
@@ -1,5 +1,5 @@
-messyMain :: IO()
-messyMain = do
+--Main :: IO()
+Main = do
   print "Who is the mail for?"
   recipient <- getLine
   print "What is the Title?"
@@ -7,3 +7,12 @@ messyMain = do
   print "Who is the Author?"
   author <- getLine
   print ("Dear " ++ recipient ++ ",\n" ++ "Thanks fot buying " ++ title ++ "\nthanks,\n" ++ author )
+  print (createEmail recipient title author)
+
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+fromPart author = "Thanks,\n"++author
+createEmail recipient bookTitle author = toPart recipient ++
+                                         bodyPart bookTitle ++
+                                         fromPart author
+
